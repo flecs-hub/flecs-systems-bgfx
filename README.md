4dfb60c46a8dd1808c7813b2526fcd7e5c1c6084
Alexander Yevsyeyev <skohliks1@gmail.com>
2021-12-13 16:34:38 -0500

    - Added base source and Readme

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
+![Flecs App](img.png)
+
 # Flecs Systems BGFX
 
-This is a minimal example of a working bgfx system with flecs v2.
+This is a minimal example of a working bgfx system with flecs v2. It requires some of the flecs modules to run such as
+[gui](https://github.com/flecs-hub/flecs-components-gui), [input](https://github.com/flecs-hub/flecs-components-input), and [sdl2](https://github.com/flecs-hub/flecs-systems-sdl2).
+To manage BGFX with C it uses [bgfx.cmake](https://github.com/bkaradzic/bgfx.cmake)
 
 For a fully working example of bgfx+imgui (docking branch) with CMake please refer to **bgfx_imgui** branch
 
---
+++ flecs_systems_bgfx.h
@@ -0,0 +1,39 @@
+#ifndef FLECS_SYSTEMS_BGFX_H
+#define FLECS_SYSTEMS_BGFX_H
+
+#include "flecs.h"
+#include "flecs_components_gui.h"
+#include "flecs_components_input.h"
+#include "flecs_systems_sdl2.h"
+#include "bgfx/c99/bgfx.h"
+#include <SDL2/SDL_syswm.h>
+
+#ifndef flecs_systems_bgfx_STATIC
+#if flecs_systems_bgfx_EXPORTS && (defined(_MSC_VER) || defined(__MINGW32__))
+#define FLECS_SYSTEMS_BGFX_API __declspec(dllexport)
+#elif flecs_systems_bgfx_EXPORTS
+#define FLECS_SYSTEMS_BGFX_API __attribute__((__visibility__("default")))
+#elif defined _MSC_VER
+#define FLECS_SYSTEMS_BGFX_API __declspec(dllimport)
+#else
+#define FLECS_SYSTEMS_BGFX_API
+#endif
+#else
+#define FLECS_SYSTEMS_BGFX_API
+#endif
+
+typedef struct BgfxInit {
+  bgfx_init_t init;
+} BgfxInit;
+
+typedef struct FlecsSystemsBgfx {
+  ECS_DECLARE_COMPONENT(BgfxInit);
+} FlecsSystemsBgfx;
+
+FLECS_SYSTEMS_BGFX_API
+void FlecsSystemsBgfxImport(ecs_world_t *world);
+
+#define FlecsSystemsBgfxImportHandles(handles)                                 \
+  ECS_IMPORT_COMPONENT(handles, BgfxInit);
+
+#endif
